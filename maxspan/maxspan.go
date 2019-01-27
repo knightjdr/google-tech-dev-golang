90a226502ea4f4c69e17cea6e8d9d10a68c1bd1c
James Knight <knightjdr@gmail.com>
2019-01-27 18:29:27 -0500

    Add maxSpan solution for slices of any type

--- maxspan/maxspan.go
+++ maxspan/maxspan.go
@@ -1,8 +1,35 @@
 // maxspan is my solution to https://codingbat.com/prob/p189576
 package maxspan
 
-func maxSpan(arr []int) int {
-	// Find first and last index for each value in the array.
+// maxSpan works for slices of any type
+func maxSpan(arr []interface{}) int {
+	// Find first and last index for each value in the slice.
+	span := make(map[interface{}][]int)
+	for index, value := range arr {
+		if _, ok := span[value]; ok {
+			span[value][1] = index
+		} else {
+			span[value] = make([]int, 2)
+			span[value][0] = index
+		}
+	}
+
+	// Find the greatest span between values, including last occurrence.
+	max := 1
+	for _, tuple := range span {
+		if len(tuple) == 2 {
+			tupleSpan := tuple[1] - tuple[0] + 1
+			if tupleSpan > max {
+				max = tupleSpan
+			}
+		}
+	}
+	return max
+}
+
+// maxSpanInt only works for integer slices
+func maxSpanInt(arr []int) int {
+	// Find first and last index for each value in the slice.
 	span := make(map[int][]int)
 	for index, value := range arr {
 		if _, ok := span[value]; ok {
@@ -13,11 +40,11 @@ func maxSpan(arr []int) int {
 		}
 	}
 
-	// Find the greatest span between values.
+	// Find the greatest span between values, including last occurrence.
 	max := 1
 	for _, tuple := range span {
 		if len(tuple) == 2 {
-			tupleSpan := tuple[1] + 1 - tuple[0]
+			tupleSpan := tuple[1] - tuple[0] + 1
 			if tupleSpan > max {
 				max = tupleSpan
 			}
--- maxspan/maxspan_test.go
+++ maxspan/maxspan_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestMaxSpan(t *testing.T) {
+	// TEST: integer slice
 	inputs := [][]int{
 		[]int{1},
 		[]int{1, 2, 1, 1, 3},
@@ -15,6 +16,17 @@ func TestMaxSpan(t *testing.T) {
 	}
 	want := []int{1, 4, 6, 6}
 	for index, input := range inputs {
-		assert.Equal(t, want[index], maxSpan(input), "Max span between to array elements should be returned")
+		assert.Equal(t, want[index], maxSpanInt(input), "Max span between to integer slice elements should be returned")
+	}
+
+	// TEST: slice of any type
+	inputsInterface := [][]interface{}{
+		[]interface{}{1, 4, 2, 1, 4, 1, 4},
+		[]interface{}{"a", "b", "c", "d", "a", "c"},
+		[]interface{}{true, true, true, false, false, true},
+	}
+	want = []int{6, 5, 6}
+	for index, input := range inputsInterface {
+		assert.Equal(t, want[index], maxSpan(input), "Max span between to slice elements should be returned")
 	}
 }
