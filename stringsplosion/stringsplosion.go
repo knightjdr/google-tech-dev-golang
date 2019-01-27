e6ac9e52ba5b6811c4e3c72de1b9de82b8b90307
James Knight <knightjdr@gmail.com>
2019-01-26 23:06:11 -0500

    Add UTF8 only stringSplosion solution

--- stringsplosion/stringsplosion.go
+++ stringsplosion/stringsplosion.go
@@ -1,8 +1,11 @@
 // stringsplosion is my solution to https://codingbat.com/prob/p117334
 package stringsplosion
 
-import "strings"
+import (
+	"strings"
+)
 
+// stringSplosion can process strings with non UTF-8 characters
 func stringSplosion(str string) string {
 	var exploded strings.Builder
 	splitString := strings.Split(str, "")
@@ -11,4 +14,13 @@ func stringSplosion(str string) string {
 		exploded.Write(bytes)
 	}
 	return exploded.String()
+}
+
+// stringSplosionUTF can only process strings with UTF-8 characters
+func stringSplosionUTF(str string) string {
+	byteArray := make([]byte, 0)
+	for index := range str {
+		byteArray = append(byteArray, []byte(str[0:index+1])...)
+	}
+	return string(byteArray)
 }
--- stringsplosion/stringsplosion_test.go
+++ stringsplosion/stringsplosion_test.go
@@ -7,9 +7,17 @@ import (
 )
 
 func TestStringsplosion(t *testing.T) {
-	inputs := []string{"Code", "abc", "ab"}
-	want := []string{"CCoCodCode", "aababc", "aab"}
+	// TEST: strings possibly containing non UTF-8 characters
+	inputs := []string{"Code", "abc", "äb"}
+	want := []string{"CCoCodCode", "aababc", "ääb"}
 	for index, input := range inputs {
 		assert.Equal(t, want[index], stringSplosion(input), "String should explode correctly")
+	}
+
+	// TEST: strings possibly containing non UTF-8 characters
+	inputs = []string{"Code", "abc", "ab"}
+	want = []string{"CCoCodCode", "aababc", "aab"}
+	for index, input := range inputs {
+		assert.Equal(t, want[index], stringSplosionUTF(input), "String should explode correctly")
 	}
 }
